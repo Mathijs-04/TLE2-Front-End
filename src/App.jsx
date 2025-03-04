d38f0228327be3f9cfb9363a0ea287811122b835
Mathijs-04 <mathijsvandermeijde@gmail.com>
2025-03-04 15:21:21 +0100

    Updated header.jsx layout and added logo's

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router';
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import Home from "./Home.jsx";
 import Theorie from "./Theorie.jsx";
 import Oefenen from "./Oefenen.jsx";
 import Navigation from "./components/Navigation.jsx";
+import Header from "./components/Header.jsx";
 
 function App() {
     return (
         <Router>
+            <Header />
             <Routes>
-                <Route path="/" element={<Navigation/>}>
-                    <Route index element={<Home/>}/>
-                    <Route path="theorie" element={<Theorie/>}/>
-                    <Route path="oefenen" element={<Oefenen/>}/>
+                <Route path="/" element={<Navigation />}>
+                    <Route index element={<Home />} />
+                    <Route path="theorie" element={<Theorie />} />
+                    <Route path="oefenen" element={<Oefenen />} />
                 </Route>
             </Routes>
         </Router>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
-function Header () {
-    return(
-        <>
-        </>
-    )
+function Header() {
+    return (
+        <header className="flex justify-between items-center p-4">
+            <img src="/public/hr.svg" alt="HR-logo" className="h-8 w-8 ml-4" />
+            <img src="/public/profile.svg" alt="Profile" className="h-8 w-8 mr-4" />
+        </header>
+    );
 }
 
 export default Header;
