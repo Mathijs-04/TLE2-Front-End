a2d0a14e907c0d2783f9b10346335f468eae6484
Mathijs-04 <mathijsvandermeijde@gmail.com>
2025-03-05 13:15:57 +0100

    Updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 function Header() {
     return (
         <header className="flex justify-between items-center p-4">
-            <img src="/public/hr.svg" alt="HR-logo" className="h-8 w-8 ml-4" />
-            <img src="/public/profile.svg" alt="Profile" className="h-8 w-8 mr-4" />
+            <img src="/hr.svg" alt="HR-logo" className="h-8 w-8 ml-4" />
+            <img src="/profile.svg" alt="Profile" className="h-8 w-8 mr-4" />
         </header>
     );
 }
