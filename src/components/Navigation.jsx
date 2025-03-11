285dcf3d97d8cf7c3ef97fffb443b5735c7d757a
J-Hoogvliet <justinhoogvliet@outlook.com>
2025-03-11 11:40:40 +0100

    Edit - Style navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,32 +3,33 @@ import {Link, NavLink, Outlet} from "react-router";
 function Navigation() {
     return (
         <>
-            <nav className="flex justify-between items-center p-4 text-white mr-10 ml-10">
-                <div className="flex space-x-4">
+            <div className="border-IceBlue border-b-4 w-11/12 mx-auto">
+                <nav className="flex justify-between items-center p-4 ">
+                    <div className="flex space-x-4">
+                        <NavLink
+                            to={"/"}
+                            className={({isActive}) =>
+                                `w-11 h-11 p-1 ${isActive ? 'shadow-md shadow-gray-500 border rounded' : ''}`}>
+                            <img src="/public/images/home.png" alt={"Link naar Home"}/>
+                        </NavLink>
+                        <NavLink
+                            to={"/alfabet"}
+                            className={({isActive}) =>
+                                `w-11 h-11 p-1 ${isActive ? 'shadow-md shadow-gray-500 border rounded' : ''}`}>
+                            <img src="/public/images/abc-block.png" alt={"Link naar Het alfabet"}
+                                 className="w-full h-full"/>
+                        </NavLink>
+                    </div>
                     <NavLink
-                        to={"/"}
+                        to={"/profile"}
                         className={({isActive}) =>
-                            `w-11 h-11 p-1 ${isActive ? 'shadow-md shadow-gray-500 border rounded' : ''}`}>
-                        <img src="/public/images/home.png" alt={"Link naar Home"}/>
+                            `w-12 h-12 p-1 ${isActive ? 'shadow-md shadow-gray-500 border rounded' : ''}`}>
+                        <img src="/public/images/user.png" alt={"Link naar jouw profiel"}/>
                     </NavLink>
-                    <NavLink
-                        to={"/alfabet"}
-                        className={({isActive}) =>
-                            `w-11 h-11 p-1 ${isActive ? 'shadow-md shadow-gray-500 border rounded' : ''}`}>
-                        <img src="/public/images/abc-block.png" alt={"Link naar Het alfabet"}
-                             className="w-full h-full"/>
-                    </NavLink>
-                </div>
-                <NavLink
-                    to={"/profile"}
-                    className={({isActive}) =>
-                        `w-12 h-12 p-1 ${isActive ? 'shadow-md shadow-gray-500 border rounded' : ''}`}>
-                    <img src="/public/images/user.png" alt={"Link naar jouw profiel"}/>
-                </NavLink>
-            </nav>
-            <div className={"flex justify-center items-center"}>
-                <div className="w-11/12 border-t-4 border-blue-300 my-4  "></div>
+                </nav>
             </div>
+
+
             <main>
                 <Outlet/>
             </main>
