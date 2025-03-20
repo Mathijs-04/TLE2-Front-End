0cb03386a987f7f8ccd3a506fb4dcd4bba2875d3
Mathijs-04 <mathijsvandermeijde@gmail.com>
2025-03-20 13:13:41 +0100

    Updated login redirect path to server

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -84,7 +84,7 @@ function Profile() {
         const currentUrl = location.pathname;
         if (token === null) {
             localStorage.setItem("redirectUrl", currentUrl);
-            window.location.href = 'https://cmgt.hr.nl/chat-login/handle/%7Bapplication%7D?redirect=http://localhost:5173/login';
+            window.location.href = 'https://cmgt.hr.nl/chat-login/handle/%7Bapplication%7D?redirect=http://http://145.24.222.170/login';
         }
     }, [navigate]);
 
